8875f1e6dbc303c041cab313cc085c5e33492557
hgokhale <hemant.gokhale@coursehero.com>
2024-08-16 21:44:07 -0700

    1. For Path.walk(), dirs needs to be updated in place. 2. Created different functions for printing different types of results. 3. Extracted into functions the logic for including directories and files in search for images.

--- images.py
+++ images.py
@@ -19,31 +19,56 @@ class Image:
 def include_dir_in_search(entry: str) -> bool:
     return entry not in ["Pods", "Images.xcassets"] and not entry.startswith(".") and not entry.endswith(".framework")
 
+
+def include_file_in_search(local_path: Path) -> bool:
+    return (local_path.is_file()
+            and not local_path.name.startswith(".")
+            and local_path.suffix not in [".plist", ".md", ".lock", ".pbxproj", ".js"]
+            and local_path.name not in ["Podfile"])
+
+
+def print_unusual_locations(local_images: List[Image]):
+    for local_image in local_images:
+        if local_image.occurrences and local_image.occurrences[0].suffix not in [".swift", ".m", ".xib", ".storyboard"]:
+            print(f"{local_image.path}, {local_image.occurrences[0]}")
+
+
+def print_all_images(local_images: List[Image]):
+    for local_image in local_images:
+        print(f"{local_image.path}, {str(local_image.occurrences[0]) if local_image.occurrences else None}")
+
+
+def print_unused_images(local_images: List[Image]):
+    for local_image in local_images:
+        if local_image.is_unused:
+            print(f"{local_image.path}, {str(local_image.occurrences[0]) if local_image.occurrences else None}")
+
+
 start_time = time.time()
 project_root = Path.home().joinpath("work/ios")
 images = [Image(p=entry) for entry in project_root.rglob("*.imageset") if "Pods" not in entry.parts]
 
 # Search for occurrence of each image in all files
 for root, dirs, files in project_root.walk():
-    dirs = [d for d in dirs if include_dir_in_search(d)]
+    dirs[:] = [d for d in dirs if include_dir_in_search(d)]
     for file in files:
         path = root / file
-        if path.is_file():
-            try:  # Skip non-text files
+        if include_file_in_search(path):
+            try:
                 with path.open() as f:
                     text = f.read()
                     for image in [i for i in images if i.is_unused]:
-                        if image.name in text:
+                        if f'"{image.name}"' in text:
                             image.occurrences.append(path)
             except UnicodeDecodeError:
-                continue
+                continue  # Skip non-text files
 
 # Identify unused images and sort them for printing
-unused_images = [i for i in images if not i.is_unused]
-unused_images.sort(key=lambda x: (x.path, x.name))
+unused_images = [i for i in images if i.is_unused]
 print(
-    f"Out of {len(images)} total images, found {len(unused_images)} unused images occupying {sum(i.disk_usage for i in unused_images):,} bytes on disk.")
-[print(f"{i.path}, {i.disk_usage / 1000:.2f} kB") for i in unused_images]
+    f"Total images: {len(images)}, unused: {len(unused_images)}, occupying {sum(i.disk_usage for i in unused_images):,} bytes on disk.")
+
+print_unusual_locations(images)
 
 end_time = time.time()
 print(f"Time taken: {end_time - start_time} seconds")
